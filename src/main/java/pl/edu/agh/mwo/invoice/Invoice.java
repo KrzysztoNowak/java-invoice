649ae22df3298e8d599528fa5f2c031aca8b4061
Elandir <krzysztofn@student.agh.edu.pl>
2022-02-05 16:23:27 +0100

    8testów z InvoceTest na zielono

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -1,30 +1,49 @@
 package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
-import java.util.Collection;
+import java.util.*;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
-    private Collection<Product> products;
+    private Map<Product, Integer> products = new HashMap<Product, Integer>();
 
     public void addProduct(Product product) {
-        // TODO: implement
+        this.addProduct(product,1);
     }
 
     public void addProduct(Product product, Integer quantity) {
-        // TODO: implement
+        this.products.put(product, quantity);
     }
 
     public BigDecimal getSubtotal() {
-        return null;
+        BigDecimal sum = BigDecimal.ZERO;
+        for(Product product: this.products.keySet()) {
+            Integer quantity = this.products.get(product);
+            final BigDecimal quantityAsBigDecimal = BigDecimal.valueOf(quantity);
+            sum = sum.add(product.getPrice().multiply(quantityAsBigDecimal));
+        }return sum;
     }
 
+
     public BigDecimal getTax() {
-        return null;
+//        BigDecimal tax = BigDecimal.ZERO;
+//        for(Product product: this.products.keySet()) {
+//            Integer quantity = this.products.get(product);
+//            final BigDecimal quantityAsBigDecimal = BigDecimal.valueOf(quantity);
+//            tax = tax.add(product.getPrice().multiply(quantityAsBigDecimal));
+//        }return tax;
+        return BigDecimal.ZERO;
     }
 
     public BigDecimal getTotal() {
-        return null;
+        BigDecimal total = BigDecimal.ZERO;
+        for(Product product: this.products.keySet()) {
+            Integer quantity = this.products.get(product);
+            final BigDecimal quantityAsBigDecimal = BigDecimal.valueOf(quantity);
+            total = total.add(product.getPrice().multiply(quantityAsBigDecimal));
+        }return total;
+
+
     }
 }
